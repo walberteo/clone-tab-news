4898b7cc68f67f28917428c08cb0a7f5fda3ce23
Walber Teodoro <walberteo@gmail.com>
2025-11-06 23:30:35 +0000

    building `staging`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Yu eu amo você. Se você me ama da uma risadinha 😎 </h1>;
+  return <h1> Yu eu amo você. Se você me ama da uma risadinha 😎 </h1>;
 }
 
 export default Home;
